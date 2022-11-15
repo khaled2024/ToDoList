f5ecfe4defa432efd357581a6ea0d97d3b8dc9e7
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2022-11-15 02:18:45 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by KhaleD HuSsien on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
